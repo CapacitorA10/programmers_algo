e246a75941bb6776517116f95f003422c9dfec96
CapacitorA10 <black12800@gmail.com>
2023-07-27 21:48:09 +0900

    chaptor 1 update
    CH1 updated

---
+++ ch01/hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    vector<string> greetings = {"Hello", "Hola", "Bonjour", "Ciao"};
+
+    for (const auto& greeting : greetings) {
+        cout << greeting << ", World!" << endl;
+    }
+    cout << std::size(greetings) << endl;
+    return 0;
+}
