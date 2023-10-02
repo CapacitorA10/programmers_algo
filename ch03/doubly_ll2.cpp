3e05b6ad2646e1069e3d30e6fd0e8bbc7328411c
CapacitorA10 <black12800@gmail.com>
2023-10-02 17:11:26 +0900

    향상된 연결 리스트 강의 내용
    어렵당 ㅜㅜ

---
+++ ch03/doubly_ll2.cpp
@@ -0,0 +1,144 @@
+#include <iostream>
+
+struct Node
+{
+    int data;
+    Node *prev;
+    Node *next;
+};
+
+class DoublyLinkedList
+{
+private:
+    int count;
+    Node *header;
+    Node *trailer;
+
+public:
+    class Iterator
+    {
+    private:
+        Node *ptr;
+
+    public:
+        Iterator(): ptr(nullptr) {} // 기본 생성자
+        Iterator(Node *p): ptr(p) {} // 생성자
+
+        int& operator*() { return ptr->data; } // 역참조 연산자, data를 참조 형태로 반환
+
+        Iterator& operator++() { // 전위 증가 연산자, ++poo로 작동, poo++로 작동하게 하려면 int operator++(int)을 작성
+            ptr = ptr->next;
+            return *this;
+        }
+        Iterator& operator--() { // 전위 감소 연산자, --poo로 작동, poo--로 작동하게 하려면 int operator--(int)을 작성
+            ptr = ptr->prev;
+            return *this;
+        }
+        bool operator==(const Iterator& other) const { // 비교 연산자
+            return ptr == other.ptr;
+        }
+        bool operator!=(const Iterator& other) const { // 비교 연산자
+            return ptr != other.ptr;
+        }
+
+        friend class DoublyLinkedList;
+    };
+
+    DoublyLinkedList()
+    {
+        count = 0;
+        header = new Node{0, nullptr, nullptr};
+        trailer = new Node{0, nullptr, nullptr};
+        header->next = trailer;
+        trailer->prev = header;
+    }
+    ~DoublyLinkedList()
+    {
+        while (!empty())
+        {
+            pop_front();
+        }
+        delete header;
+        delete trailer;
+    }
+
+    Iterator begin() const { return Iterator(header->next); } 
+    Iterator end() const { return Iterator(trailer); }
+    // the reason of const is that begin(),end() is not supposed to change the object
+    // but the return value is not const because we want to change the value of the object
+
+    void insert(const Iterator& pos, const int val) 
+    {
+        Node *p = pos.ptr;
+        Node *new_node = new Node{val, p->prev, p};
+        new_node->prev->next = new_node;
+        p->prev = new_node;
+        ++count;
+    }
+    void push_front(const int val)
+    {
+        insert(begin(), val);
+    }
+    void push_back(const int val)
+    {
+        insert(end(), val);
+    }
+    void erase(const Iterator& pos)
+    {
+        Node *p = pos.ptr;
+        p->prev->next = p->next;
+        p->next->prev = p->prev;
+        delete p;
+        --count;
+    }
+    void pop_front()
+    {
+        erase(begin());
+    }
+    void pop_back()
+    {
+        erase(--end());
+    }
+
+    Iterator find(const int val) const
+    {
+        Node *cursor = header->next;
+        while (cursor != trailer)
+        {
+            if (cursor->data == val)
+            {
+                return Iterator(cursor);
+            }
+            cursor = cursor->next;
+        }
+        return end();
+    }
+
+    bool empty()
+    {
+        return count == 0;
+    }
+    int size()
+    {
+        return count;
+    }
+};
+
+int main()
+{
+    DoublyLinkedList list;
+    list.push_front(10);
+    list.push_front(20);
+    list.push_front(30);
+    list.push_back(-10);
+    // list: header => 30 => 20 => 10 => -10 => trailer
+    auto it = list.find(20);
+    if (it != list.end()) {
+        std::cout << "Found: " << *it << std::endl;
+        list.insert(it, 25);
+    }
+    // list: header => 30 => 25 => 20 => 10 => -10 => trailer
+    for (auto n : list) {
+        std::cout << n << ", ";
+    } std::cout << std::endl;
+}
